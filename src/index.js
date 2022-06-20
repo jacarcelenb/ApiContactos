9eedf41d498aa288cb9a3f28262e1f86d10ac294
jacarcelenb <carcelenjorge17@gmail.com>
2022-06-19 21:48:39 -0500

    CORS configuracion en la API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+// cors
+app.use(cors())
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
